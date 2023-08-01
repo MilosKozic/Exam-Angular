08406e30200797424e25709ae49d36746284a200
MilosKozic <milos.kozic1986@gmail.com>
2023-08-01 13:10:14 +0200

    Add main module, and set landing page

--- super-admin-app/src/app/app-routing.module.ts
+++ super-admin-app/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './users/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch:'full' },
-  { path: 'home', component: HomeComponent },
   {
     path: 'auth',
     loadChildren: () => import('./authentication/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule)
   },
   { path: '**', redirectTo: 'auth/login' },
 ];
---
+++ super-admin-app/src/app/main/main.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { ButtonModule } from "../shared/button/button.module";
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { MainRoutingModule } from "./main-routing.module";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { MainComponent } from './main.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavbarComponent,
+    MainLayoutComponent,
+    MainComponent
+  ],
+  imports: [MainRoutingModule, ButtonModule],
+  exports: []
+})
+export class MainModule { }
